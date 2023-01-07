9e0259717a9b9746754e59e0943c78c3220cbcc0
Ronit Panda <pandaronit25@gmail.com>
2023-01-07 17:45:40 +0530

    feat: added deployment workflows

--- client/utils/build-client.ts
+++ client/utils/build-client.ts
@@ -7,7 +7,7 @@ export const buildClient = (
     if (typeof window === 'undefined') {
         return axios.create({
             baseURL:
-                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+                'http://www.micro-insta.ronit.pro',
             headers: req!.headers,
         });
     } else {
